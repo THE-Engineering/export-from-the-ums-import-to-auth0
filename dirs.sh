f2b141401775e9c647b95d6aa2d2a2348cc9acc9
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-04-09 17:35:09 +0100

    `dirs`

--- dirs.sh
+++ dirs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HARD=false
+SOFT=true
+
 has_archive_directory_pattern () {
   [[ "$1" =~ [0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]+ ]]
 }
@@ -20,11 +23,57 @@ dirs () {
   done
 }
 
-echo ✨
-dirs ./json
-dirs .platform
-dirs .users-exports
-dirs .users-imports
-dirs .validate
-echo 👋
-exit 0
+for flag in "$@";
+do
+  shift
+  case "$flag" in
+    '--hard')   set -- "$@" '-h'   ;;
+    '--soft')   set -- "$@" '-s'   ;;
+    *)          set -- "$@" "$flag" ;;
+  esac
+done
+
+while getopts 'h,s' flag;
+do
+  case "${flag}" in
+    h)
+      HARD=true
+      SOFT=false
+      ;;
+    s)
+      HARD=false
+      SOFT=true
+      ;;
+    *) exit 1;;
+  esac
+done
+
+if $HARD ;
+then
+  echo ✨
+  rm -rf ./json
+  rm -rf .platform
+  rm -rf .users-exports
+  rm -rf .users-imports
+  rm -rf .validate
+  echo 👋
+  exit 0
+else
+  if $SOFT ;
+  then
+    echo ✨
+    dirs ./json
+    dirs .platform
+    dirs .users-exports
+    dirs .users-imports
+    dirs .validate
+    echo 👋
+    exit 0
+  else
+    echo 💩
+    exit 1
+  fi
+fi
+
+echo 💥
+exit 1
